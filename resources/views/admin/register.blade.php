194db6a05bfac9a298a20e3fdbb041ea22c14502
Brobert04 <robert.bercaru@liceuldearte.ro>
2022-12-13 00:14:59 +0200

    Sign up template and functionality done.

--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -12,27 +12,31 @@
                     <p class="mb-0">Pentru a putea continua, te rugăm să-ți creezi un cont folosind formularul de mai jos :)</p>
                   </div>
                   <div class="card-body">
-                    <form role="form text-left">
+                    <form role="form text-left" method="POST" action="{{ route('register') }}">
+                      @csrf
                       <div class="mb-3">
                         <label>Nume</label>
-                        <input type="text" class="form-control" placeholder="Name" aria-label="Name" aria-describedby="email-addon">
+                        <input name="name" type="text" class="form-control" placeholder="Nume" aria-label="Name" aria-describedby="email-addon">
                       </div>
                       <div class="mb-3">
                         <label>Email</label>
-                        <input type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="email-addon">
+                        <input name="email" type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="email-addon">
                       </div>
                       <div class="mb-3">
                         <label>Parolă</label>
-                        <input type="password" class="form-control" placeholder="Parolă" aria-label="Password" aria-describedby="password-addon">
+                        <input name="password" type="password" class="form-control" placeholder="Parolă" aria-label="Password" aria-describedby="password-addon">
+                      </div>
+                      <div class="mb-3">
+                        <label>Confirmă Parolă</label>
+                        <input name="password_confirmation" type="password" class="form-control" placeholder="Confirmă Parola" aria-label="Password" aria-describedby="password-addon"
+                        for="password_confirmation">
                       </div>
                       <div class="form-check form-check-info text-left">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
-                        <label class="form-check-label" for="flexCheckDefault">
-                          Sunt de acord cu <a href="javascript:;" class="text-dark font-weight-bolder">Termenii și condițille</a>
-                        </label>
+                        <input name="remember" class="form-check-input" type="checkbox" id="rememberMe" checked="">
+                        <label class="form-check-label" for="rememberMe">Nu mă uita :)</label>
                       </div>
                       <div class="text-center">
-                        <button type="button" class="btn bg-gradient-dark w-100 my-4 mb-2">Înregistrare</button>
+                        <button type="submit" class="btn bg-gradient-dark w-100 my-4 mb-2">Înregistrare</button>
                       </div>
                       <p class="text-sm mt-3 mb-0">Ai deja cont?<a href="{{ route('login') }}" class="text-dark font-weight-bolder">  Folosește-l</a></p>
                     </form>
@@ -53,59 +57,5 @@
         </div>
       </section>
     </main>
-    <!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
-    {{-- <footer class="footer py-5">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 mb-4 mx-auto text-center">
-            <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-              Company
-            </a>
-            <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-              About Us
-            </a>
-            <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-              Team
-            </a>
-            <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-              Products
-            </a>
-            <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-              Blog
-            </a>
-            <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-              Pricing
-            </a>
-          </div>
-          <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
-            <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
-              <span class="text-lg fab fa-dribbble"></span>
-            </a>
-            <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
-              <span class="text-lg fab fa-twitter"></span>
-            </a>
-            <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
-              <span class="text-lg fab fa-instagram"></span>
-            </a>
-            <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
-              <span class="text-lg fab fa-pinterest"></span>
-            </a>
-            <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
-              <span class="text-lg fab fa-github"></span>
-            </a>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-8 mx-auto text-center mt-1">
-            <p class="mb-0 text-secondary">
-              Copyright © <script>
-                document.write(new Date().getFullYear())
-              </script> Soft by Creative Tim.
-            </p>
-          </div>
-        </div>
-      </div>
-    </footer> --}}
-    <!-- -------- END FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
-    </main>
+  </main>
 @endsection
